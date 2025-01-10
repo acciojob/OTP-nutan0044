155c2830636960bbe65d0dcd5022ad60cef0ec25
nutan0044 <nutannagargoje73@gmail.com>
2025-01-10 22:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function moveFocus(currentInput, direction) {
+    if (currentInput.value.length >= currentInput.maxLength) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
+
+function handleBackspace(event, currentInput) {
+    if (event.key === 'Backspace' && currentInput.value === '') {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput) {
+            previousInput.focus();
+        }
+    }
+}
